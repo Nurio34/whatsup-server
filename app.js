b19f61f7d118d6fdc86c6194a58620c0f97b8a30
Nurio34 <nurioonsoftware@gmail.com>
2024-11-19 01:16:35 +0300

    Fix_VercelIntegrate

--- app.js
+++ app.js
@@ -21,7 +21,10 @@ app.use(cookieParser());
 
 app.use(
     cors({
-        origin: ["http://localhost:3000"],
+        origin: [
+            "http://localhost:3000",
+            "https://whatsup-lime-rho.vercel.app",
+        ],
         credentials: true,
     }),
 );
