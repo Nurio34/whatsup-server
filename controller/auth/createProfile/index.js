cc5db1e7948b6b2f52ab7d23e213969d93343fad
Nurio34 <nurioonsoftware@gmail.com>
2024-12-09 19:45:56 +0300

    fix_createProfile

--- controller/auth/createProfile/index.js
+++ controller/auth/createProfile/index.js
@@ -6,52 +6,50 @@ const createProfile = async (req, res, next) => {
   const { id, name, about } = req.body;
   const { path } = req.file;
 
-  try {
-    const { url, public_id, asset_id } = await uploadAvatar(path);
+  const { url, public_id, asset_id } = await uploadAvatar(path);
 
-    const user = await User.findByIdAndUpdate(
-      id,
-      {
-        username: name,
-        about,
-        newUser: false,
-        avatar: {
-          url,
-          public_id,
-          asset_id,
-        },
+  const user = await User.findByIdAndUpdate(
+    id,
+    {
+      username: name,
+      about,
+      newUser: false,
+      avatar: {
+        url,
+        public_id,
+        asset_id,
       },
-      { new: true, runValidators: true }
+    },
+    { new: true, runValidators: true }
+  );
+
+  if (!user) {
+    return next(
+      new AppError(
+        "Something went wrong while creating proile. Please try again !"
+      )
     );
-
-    if (!user) {
-      return next(
-        new AppError(
-          "Something went wrong while creating proile. Please try again !"
-        )
-      );
-    }
-
-    const userToSendClient = {
-      id: user.id,
-      username: user.username,
-      email: user.email,
-      isVerified: user.isVerified,
-      newUser: user.newUser,
-      avatar: user.avatar,
-      createdAt: user.createdAt,
-      about: user.about,
-      status: user.status,
-    };
-
-    return res.status(200).json({
-      status: "success",
-      message: "Profile created successfully ...",
-      user: userToSendClient,
-    });
-  } catch (error) {
-    return next(new AppError("Unexpected server error !", 500));
   }
+
+  const userToSendClient = {
+    id: user.id,
+    username: user.username,
+    email: user.email,
+    isVerified: user.isVerified,
+    newUser: user.newUser,
+    avatar: user.avatar,
+    createdAt: user.createdAt,
+    about: user.about,
+    status: user.status,
+  };
+
+  console.log(userToSendClient);
+
+  return res.status(200).json({
+    status: "success",
+    message: "Profile created successfully ...",
+    user: userToSendClient,
+  });
 };
 
 module.exports = createProfile;
