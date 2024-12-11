bb4bd39dd64e72465ff3b4be8a5b6209d56a170c
Nurio34 <nurioonsoftware@gmail.com>
2024-12-11 03:58:01 +0300

    fix_VerifyEmail

--- controller/auth/verifyEmail/verifyOtp.js
+++ controller/auth/verifyEmail/verifyOtp.js
@@ -3,46 +3,50 @@ const User = require("../../../model/user");
 const createCookieAndSend = require("../../../utils/createCookieAndSend");
 
 const verifyOtp = async (req, res, next) => {
-    console.log("verifyOtp middleware");
+  console.log("verifyOtp middleware");
 
-    const user = req.user;
-    const { otp } = req.body;
-    const userOtp = user.otp;
-    const otpExpires = user.otpExpires;
+  const { id, otp } = req.body;
 
-    if (!otp) return next(new AppError("Otp is missing !", 400));
+  const user = await User.findById(id);
 
-    if (otp.length < 4)
-        return next(new AppError("Otp must be at leat 'four' number !", 400));
+  if (!user) {
+    return next(new AppError("User not found", 404));
+  }
 
-    if (otp !== userOtp) return next(new AppError("Invalid Otp", 400));
+  const userOtp = user.otp;
+  const otpExpires = user.otpExpires;
 
-    if (Date.now() > otpExpires)
-        return next(
-            new AppError("Otp has expired. Please request new Otp !", 400),
-        );
+  if (!otp) return next(new AppError("Otp is missing !", 400));
 
-    const verifiedUser = await User.findByIdAndUpdate(
-        user._id,
-        {
-            isVerified: true,
-            $unset: {
-                passwordConfirm: "",
-                otp: "",
-                otpExpires: "",
-            },
-        },
-        { new: true },
-    );
+  if (otp.length < 4)
+    return next(new AppError("Otp must be at leat 'four' number !", 400));
 
-    if (!verifiedUser) return next(new AppError("User not found !", 404));
+  if (otp !== userOtp) return next(new AppError("Invalid Otp", 400));
 
-    createCookieAndSend(
-        verifiedUser,
-        res,
-        200,
-        "Email is verified successfully ...",
-    );
+  if (Date.now() > otpExpires)
+    return next(new AppError("Otp has expired. Please request new Otp !", 400));
+
+  const verifiedUser = await User.findByIdAndUpdate(
+    user._id,
+    {
+      isVerified: true,
+      $unset: {
+        passwordConfirm: "",
+        otp: "",
+        otpExpires: "",
+      },
+    },
+    { new: true }
+  );
+
+  if (!verifiedUser) return next(new AppError("User not found !", 404));
+
+  createCookieAndSend(
+    verifiedUser,
+    res,
+    200,
+    "Email is verified successfully ..."
+  );
 };
 
 module.exports = verifyOtp;
